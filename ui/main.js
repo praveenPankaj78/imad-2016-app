dc0f4b1e9e2e4ceb83eb0901598d2dbda1751db4
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-09-26 09:23:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
+
+//Change HTML Content
 var element = document.getElementById('main-text');
 console.log(element);
 element.innerHTML = 'New Value';
+
+//Make Image move
+var img = document.getElementById('madi');
+
+img.onClick = function() {
+    img.style.marginLeft='100px';
+};
