09c8b32812fe519f709eb7a235fa30d604d055cd
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-09-29 12:24:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('submit-name/:name', function(req, res) {
+   // Get the name from the request
+   var name = req.params.name;
+   
+   names.push(name);
+   // JSON: Javacript Object Notation
+   res.send(JSON.stringify(names)); // TODO
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
