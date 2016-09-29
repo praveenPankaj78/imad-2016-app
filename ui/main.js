8016dc598bc43e3cad57157c5945757081b21811
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-09-29 11:53:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button = document.getElementById('counter');
 
 button.onclick = function () {
 
-    // Make a request to the counter endpoint
+    // Create a request object
     var request = new XMLHttpRequest();
 
     // Capture the response and store it in a variable
@@ -22,5 +22,21 @@ button.onclick = function () {
   //Make the request
   request.open('GET', 'http://praveenpankaj78.imad.hasura-app.io/counter', true);
   request.send(null);
+};
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //Make a request to the server and send the name
     
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list='';
+    for (var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
