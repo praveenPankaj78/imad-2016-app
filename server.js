2030a6081568aefe4f0ea1d984f1b62421729fab
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-10-24 15:14:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'praveenpankaj78',
+    database: 'praveenpankaj78',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -86,14 +96,29 @@ function createTemplate (data) {
     
 }
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function(err, result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       }
+       else {
+           res.send(JSON.stringify(result));
+       }
+   });
+});
+
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var names = [];
