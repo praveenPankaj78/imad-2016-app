d8831243b49d3cfad9f8e17370c259459d734d40
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-11-11 15:30:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/login', function(req, res) {
        }
        else {
            if(result.rows.length === 0){
-               res.send(username).send('username/password is invalid');
+               res.send(403).send('username/password is invalid');
                //alert("1st");
            } else {
                // Match the password
