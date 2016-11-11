6443fc7859fa841e6f407fdfb645578ac92bff3a
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2016-11-11 16:14:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,10 @@ app.post('/create-user', function(req, res) {
 app.post('/login', function(req, res) {
    var username = req.body.username;
    var password = req.body.password;
+   username = "'" + username + "'";
    //alert("Outside");
       //pool.query('SELECT * from "user" where username=$1', [username], function(err, result) {
-      pool.query('SELECT * from "user" where username=$1', [luffy], function(err, result) {
+      pool.query('SELECT * from "user" where username=$1', [username], function(err, result) {
         if(err) {
            res.status(500).send(err.toString());
        }
